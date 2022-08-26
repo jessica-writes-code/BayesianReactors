576fca4a5f962b33ebcd575fa809b4b99c0e1ba6
Jessica Moore <jmoore523@gmail.com>
2022-08-25 20:11:15 -0400

    Finish data

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,27 +1,17 @@
+from typing import List
+import string
+
 import pandas as pd
 
 
-def percent_of_year_operational(row: pd.Series) -> float:
-    """Calculate the percent of the year that a reactor was in operation.
-    
-    Notes:
-    - We assume a reactor does not start and end operations in the same year.
-    - This calculation does not account precisely for leap years.
-    """
-    year = row['year']
+PUNCTUATION = set(string.punctuation)
 
-    # Comparison to start date
-    start_date = row['start operation']
-    if year < start_date.year:
-        return 0.0
-    if year == start_date.year:
-        return (365 - start_date.dayofyear) / 365.0
 
-    # Comparison to end date
-    end_date = row['end operation']
-    if year > end_date.year:
-        return 0.0
-    if year == end_date.year:
-        return end_date.dayofyear / 365.0
-
-    return 1.0
+def split_name_fn(common_words: List[str]):
+    def split_name(x):
+        x = ''.join(ch for ch in x if ch not in PUNCTUATION)
+        x_split = x.upper().split()
+        name = ' '.join([w for w in x_split if w not in common_words])
+        number = x_split[-1] if x_split[-1].isnumeric() else None
+        return (name, number)
+    return split_name
